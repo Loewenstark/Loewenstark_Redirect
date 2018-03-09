370b5b4086cf9c0100f289528d652c9e45f874d1
Adam Varga <adam@mage-profis.de>
2018-03-09 10:41:03 +0100

    Exist main_product_id fix

--- app/code/local/Loewenstark/Redirect/Model/Observer.php
+++ app/code/local/Loewenstark/Redirect/Model/Observer.php
@@ -1,50 +1,49 @@
 <?php
 
-class Loewenstark_Redirect_Model_Observer
-{
+class Loewenstark_Redirect_Model_Observer {
     /*
      * Redirect unvisible configurable child URLs to parent with params
      */
-    public function main()
-    {
+
+    public function main() {
 
         $config = Mage::getStoreConfig('ls_redirect/general/redirect');
 
-        if ($config)
-        {
+        if ($config) {
             $this->redirectToParent();
-        } else
-        {
+        } else {
             return false;
         }
     }
 
-    public function redirectToParent()
-    {
+    public function redirectToParent() {
         $path_key = Mage::app()->getRequest()->getOriginalPathInfo();
-        $path_key = ltrim($path_key,'/');
+        $path_key = ltrim($path_key, '/');
 
         $rewrite = Mage::getModel('core/url_rewrite')
                 ->setStoreId(Mage::app()->getStore()->getId())
                 ->loadByRequestPath($path_key);
         $productId = $rewrite->getProductId();
-                
-        if(!$productId)
+
+        if (!$productId)
             return false;
-        
+
         $child = Mage::getModel('catalog/product')->setStoreId(Mage::app()->getStore()->getId())->load($productId);
 
-        if ($child && $child->getId())
-        {            
+        if ($child && $child->getId()) {
             $main_product_ids = Mage::getModel('catalog/product_type_configurable')->getParentIdsByChild($child->getId());
-            if($main_product_ids && count($main_product_ids) > 0)
+
+            if (!$main_product_id)
+                return false;
+
+            if ($main_product_ids && count($main_product_ids) > 0)
                 $main_product_id = $main_product_ids[0];
-            
+
             $product = Mage::getModel('catalog/product')->setStoreId(Mage::app()->getStore()->getId())->load($main_product_id);
             $attributes = $product->getTypeInstance(true)->getConfigurableAttributesAsArray($product);
+
             $params = array();
-            foreach ($attributes as $attribute)
-            {
+            foreach ($attributes as $attribute) {
                 $attribute_id = $attribute['attribute_id'];
                 $options_id = $child->getData($attribute['attribute_code']);
                 $params[] = $attribute_id . '=' . $options_id;
@@ -54,8 +53,7 @@ class Loewenstark_Redirect_Model_Observer
             header("HTTP/1.1 301 Moved Permanently");
             header("location:" . $result_url);
             exit;
-        } else
-        {
+        } else {
             return false;
         }
     }
